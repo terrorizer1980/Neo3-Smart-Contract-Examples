17c378bde4cb81f660b19bc1cac179a002d3bea5
陈志同 <chenzhitong12@hotmail.com>
2020-03-11 11:18:58 +0800

    添加项目文件。

---
+++ NEP5/Helper.cs
@@ -0,0 +1,16 @@
+﻿using Neo.SmartContract.Framework;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace NEP5
+{
+    public static class Helper
+    {
+        public static BigInteger TryToBigInteger(this byte[] value)
+        {
+            return value?.ToBigInteger() ?? 0;
+        }
+    }
+}
