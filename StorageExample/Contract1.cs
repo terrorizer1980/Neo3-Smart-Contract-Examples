da105391d4b9a9d213af5c28b30a1f4e3106dc22
陈志同 <chenzhitong12@hotmail.com>
2020-03-25 11:10:18 +0800

    Update Contract1.cs

--- StorageExample/Contract1.cs
+++ StorageExample/Contract1.cs
@@ -10,8 +10,8 @@ namespace StorageExample
     [Features(ContractFeatures.HasStorage)]
     public class Contract1 : SmartContract
     {
-        [DisplayName("documentSaved")]
-        public static event Action<byte[], uint> DocumentSaved;
+        [DisplayName("saved")]
+        public static event Action<byte[], uint> Saved;
 
         /// <summary>
         /// Hash:
@@ -43,48 +43,38 @@ namespace StorageExample
 
             var blockChainHeight = Blockchain.GetHeight();
             Hash.Put(sha256, blockChainHeight);
-            DocumentSaved(sha256, blockChainHeight);
+            Saved(sha256, blockChainHeight);
 
-            return true;
-        }
-
-        [DisplayName("bulkIssue")]
-        public static bool BulkIssue(byte[][] documents)
-        {
-            foreach (var document in documents)
-            {
-                if (document.Length != 32) return false;
-                if (IsSaved(document)) return false;
-            }
-
-            var blockChainHeight = Blockchain.GetHeight();
-            foreach (var document in documents)
-            {
-                Hash.Put(document, blockChainHeight);
-                DocumentSaved(document, blockChainHeight);
-            }
             return true;
         }
 
         [DisplayName("getSavedBlock")]
-        public static uint GetSavedBlock(byte[] document)
+        public static uint GetSavedBlock(byte[] sha256)
         {
-            if (document.Length != 32) throw new ArgumentException();
-            if (!IsSaved(document)) throw new ArgumentException();
-            return (uint)Hash.Get(document).AsBigInteger();
+            if (sha256.Length != 32) throw new ArgumentException();
+            if (!IsSaved(sha256)) throw new ArgumentException();
+            return (uint)Hash.Get(sha256).TryToBigInteger();
         }
 
         [DisplayName("isSaved")]
-        public static bool IsSaved(byte[] document)
+        public static bool IsSaved(byte[] sha256)
         {
-            if (document.Length != 32) return false;
-            return Hash.Get(document) != null;
+            if (sha256.Length != 32) return false;
+            return Hash.Get(sha256) != null;
         }
 
         [DisplayName("isSavedBefore")]
-        public static bool IsSavedBefore(byte[] document, uint blockHeight)
+        public static bool IsSavedBefore(byte[] sha256, uint blockHeight)
         {
-            return GetSavedBlock(document) < blockHeight;
+            return GetSavedBlock(sha256) < blockHeight;
+        }
+    }
+
+    public static class Helper
+    {
+        public static BigInteger TryToBigInteger(this byte[] value)
+        {
+            return value?.ToBigInteger() ?? 0;
         }
     }
 }
